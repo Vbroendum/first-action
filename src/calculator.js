63eab55f3ad166293e92f734ed0f56f75e8066cd
Victor Brøndum <vbroendum@gmail.com>
2026-03-16 10:12:02 +0100

    change calculator

--- src/calculator.js
+++ src/calculator.js
@@ -2,7 +2,7 @@ function add(a, b) {
     if (typeof a !== 'number' || typeof b !== 'number') {
         throw new Error('Inputs must be numbers');
     }
-    return a + b;
+    return a - b;
 }
 
 module.exports = { add };
